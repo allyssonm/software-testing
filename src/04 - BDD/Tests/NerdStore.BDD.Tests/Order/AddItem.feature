667cfdfaa6c077a92d68155a7136b501f17a85a0
Allysson Santos <allysson.lp@gmail.com>
2020-12-07 00:00:52 +0100

    BDD SpecFlow.

---
+++ src/04 - BDD/Tests/NerdStore.BDD.Tests/Order/AddItem.feature
@@ -0,0 +1,12 @@
+﻿Feature: Order - Add Item to Order
+	As an user
+	I want to add an item to order
+	So that I can buy it later
+
+Scenario: Add item to order sucessfully
+	Given a product is in showcase
+	And it is available on stock
+	And the user is already logged in
+	When the user adds an unit to order
+	Then the user will be redirected to the purchase summary
+	And the order's total price will be exactly the same as the added item
---
+++ src/04 - BDD/Tests/NerdStore.BDD.Tests/Order/Order_AddItemToOrderSteps.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace NerdStore.BDD.Tests.Order
+{
+    [Binding]
+    public class Order_AddItemToOrderSteps
+    {
+        [Given(@"a product is in showcase")]
+        public void GivenAProductIsInShowcase()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        
+        [Given(@"it is available on stock")]
+        public void GivenItIsAvailableOnStock()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        
+        [Given(@"the user is already logged in")]
+        public void GivenTheUserIsAlreadyLoggedIn()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        
+        [When(@"the user adds an unit to order")]
+        public void WhenTheUserAddsAnUnitToOrder()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        
+        [Then(@"the user will be redirected to the purchase summary")]
+        public void ThenTheUserWillBeRedirectedToThePurchaseSummary()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        
+        [Then(@"the order's total price will be exactly the same as the added item")]
+        public void ThenTheOrderSTotalPriceWillBeExactlyTheSameAsTheAddedItem()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
